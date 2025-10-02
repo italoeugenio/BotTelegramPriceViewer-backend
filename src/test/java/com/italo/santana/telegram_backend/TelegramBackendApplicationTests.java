45d96da399570d5897cfab7590f002c87156a669
Ítalo Santana <italoeugenio539@gmail.com>
2025-10-02 13:36:24 -0300

    init: initial commit

---
+++ src/test/java/com/italo/santana/telegram_backend/TelegramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italo.santana.telegram_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
